41fff9ecfe94f2802c9a109d92845fd7b3f77437
sferna10 <sferna10@email.cpcc.edu>
2023-09-30 23:36:57 -0400

    locating bug in highscores

--- Quiz_Challenge/Assets/js/script.js
+++ Quiz_Challenge/Assets/js/script.js
@@ -121,34 +121,17 @@ function saveHighscore() {
     window.location.href = "highscores.html";
   }
 }
-//function printHighscores() {
-  //var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-  //highscores.sort(function (a, b) {
-    //return b.score - a.score;
-  //});
+function renderMessage(){
+var lastHighScore = JSON.parse(loalStorage.getElementById("saveHighscore"));
+if (lastHighScore !== null) {
+ document.querySelector(".message").textContent= highscores + initials}
+}
 
-//for (var i = 0; i, highscores.length; i += 1) {
- //var liTag = document.createElement("li");
-//liTag.textContent = highscores[i].initials + " - " + highscores[i].score;
-
-//var olEl = document.getElementById("highscores");
-// olEl.appendChild(liTag);
-//}
-//}
-
-//function clearHighscores() {
-// window.localStorage.removeItem("highscores");
-//window.location.reload();
-//}
-
-//document.getElementById("clear").onclick = clearHighscores;
-
-//printHighscores();
 
 function checkforEnter(event) {
-if (event.key === "Enter") {
- saveHighscore();
-}
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
 }
 submitBtn.onclick = saveHighscore;
 startBtn.onclick = startQuiz;
